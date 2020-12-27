94ce1b9e35328fb1251a9a4a622f581f7af40ea1
Koimi YOVO <yovoedem33@gmail.com>
2020-12-27 19:16:22 +0100

    Remove Delimiter class and update Input

--- src/com/string_calculator/Calculator.kt
+++ src/com/string_calculator/Calculator.kt
@@ -19,55 +19,51 @@ object Calculator {
 }
 
 class Input(private var input: String) {
+    private val delimiters = mutableSetOf(",", "\n")
+
     internal fun extractNumbers(): List<Int> {
         this.convertEmptyInput()
-        val defaultDelimiters = mutableListOf(",", "\n")
-        if (this.usesCustomDelimiterSyntax()) {
-            val customDelimiter = this.extractDelimiter()
-            customDelimiter.forEach { defaultDelimiters.add(it) }
-            input = this.secondLine()
-        }
-        return input.split(defaultDelimiters).toIntList()
+        this.extractDelimiters()
+        return input.split(delimiters).toIntList()
     }
 
     private fun convertEmptyInput() = apply { if (input.isEmpty()) input = "0" }
 
+    private fun extractDelimiters() {
+        if (this.usesCustomDelimiterSyntax()) {
+            val delimiterPart = getDelimiterPart()
+            if (delimiterPart.isOfOneLength()) {
+                delimiters.add(delimiterPart)
+            } else {
+                delimiterPart.extractCustomDelimiters()
+            }
+            input = this.secondLine()
+        }
+    }
+
     private fun usesCustomDelimiterSyntax() = input.startsWith("//")
 
-    private fun extractDelimiter(): Delimiter {
-        var delimiter = Delimiter(listOf(input.substring(input.indexOf("//") + 2, input.indexOf("\n"))))
-        if (delimiter.isOfAnyLength()) {
-            val delimiters = this.allDelimiters()
-            delimiter = Delimiter(delimiters)
+    private fun getDelimiterPart() = input.substring(input.indexOf("//") + 2, input.indexOf("\n"))
+
+    private fun String.isOfOneLength() = length == 1
+
+    private fun String.extractCustomDelimiters() {
+        var delimiter = this
+        while (delimiter.containsDelimiter()) {
+            val oneDelimiter =
+                delimiter.substring(delimiter.indexOf("[") + 1, delimiter.indexOf("]"))
+            delimiters.add(oneDelimiter)
+            delimiter = delimiter.lookForNextDelimiter()
         }
-        return delimiter
     }
 
-    private fun allDelimiters(): List<String> {
-        val delimiters = mutableListOf<String>()
-        while (this.hasDelimiter()) {
-            val delimiter = input.substring(input.indexOf("[") + 1, input.indexOf("]"))
-            delimiters.add(delimiter)
-            this.lookForNextDelimiter()
-        }
-        return delimiters
-    }
+    private fun String.containsDelimiter() = contains("[")
 
-    private fun hasDelimiter() = input.contains("[")
-
-    private fun lookForNextDelimiter() {
-        input = input.substring(input.indexOf("]") + 1)
-    }
+    private fun String.lookForNextDelimiter() = this.substring(this.indexOf("]") + 1)
 
     private fun secondLine() = input.substring(input.indexOf("\n") + 1)
 
-    private fun String.split(delimiters: List<String>) = split(*delimiters.toTypedArray())
+    private fun String.split(delimiters: MutableSet<String>): List<String> = split(*delimiters.toTypedArray())
 
     private fun List<String>.toIntList(): List<Int> = map { it.toInt() }
-}
-
-class Delimiter(private val delimiters: List<String>) : Iterable<String> {
-    internal fun isOfAnyLength() = delimiters.any { it.contains("[") }
-
-    override fun iterator(): Iterator<String> = delimiters.iterator()
 }
