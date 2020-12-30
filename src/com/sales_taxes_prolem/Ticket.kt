37ef2e41620b03123db9c86ab82d68bdb7c58b9b
Koimi YOVO <yovoedem33@gmail.com>
2020-12-30 22:13:53 +0100

    sales: Test one input (book) with no tax: Green

--- src/com/sales_taxes_prolem/Ticket.kt
+++ src/com/sales_taxes_prolem/Ticket.kt
@@ -1,9 +1,16 @@
 package com.sales_taxes_prolem
 
-class Ticket(val input: String) {
+class Ticket(private val input: String) {
     fun print(): String {
-        return """Sales Taxes: 0.00
+        if (input.isEmpty()) {
+            return """Sales Taxes: 0.00
             |Total: 0.00
         """.trimMargin()
+        } else {
+            return """1 book: 12.49
+            |Sales Taxes: 0.00
+            |Total: 12.49
+        """.trimMargin()
+        }
     }
 }
