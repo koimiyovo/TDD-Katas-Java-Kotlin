6fa346589e474b45998c0284087c014767184e14
Koimi YOVO <yovoedem33@gmail.com>
2020-12-31 19:41:23 +0100

    sales: Add rounding method that rounds to the nearest 0.05

--- src/com/sales_taxes_prolem/Round.kt
+++ src/com/sales_taxes_prolem/Round.kt
@@ -1,5 +1,9 @@
 package com.sales_taxes_prolem
 
+import kotlin.math.roundToInt
+
 object Round {
-    fun roundTo2DecimalPoint(value: Float?) = "%.2f".format(value).replace(',', '.')
+    fun withFormat2DecimalPoint(value: Float) = "%.2f".format(value).replace(',', '.')
+
+    fun roundToNearest005(value: Float) = (value * 20).roundToInt() / 20.0F
 }
