1484821d66555bcfe88f2a4aa88fb719f61fd085
Koimi YOVO <yovoedem33@gmail.com>
2021-01-03 16:25:30 +0100

    money_example: Dollar equality

--- src/com/tdd_by_example/money/Dollar.kt
+++ src/com/tdd_by_example/money/Dollar.kt
@@ -4,4 +4,11 @@ class Dollar(var amount: Int) {
 
     fun times(multiplier: Int) = Dollar(amount * multiplier)
 
+    override fun equals(other: Any?): Boolean {
+        return if (other is Dollar) {
+            this.amount == other.amount
+        } else {
+            false
+        }
+    }
 }
--- src/com/tdd_by_example/money/MoneyTest.kt
+++ src/com/tdd_by_example/money/MoneyTest.kt
@@ -2,6 +2,7 @@ package com.tdd_by_example.money
 
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class MoneyTest {
     @Test
@@ -11,5 +12,10 @@ internal class MoneyTest {
         assertEquals(10, product.amount)
         product = five.times(3)
         assertEquals(15, product.amount)
+    }
+
+    @Test
+    fun `test equality`() {
+        assertTrue(Dollar(5).equals(Dollar(5)))
     }
 }
