10d2f9e8c10a8ea4b9e8c024abd98c012f8b9d0a
Koimi YOVO <yovoedem33@gmail.com>
2021-01-03 16:22:34 +0100

    money_example: Degenerate objects

--- src/com/tdd_by_example/money/Dollar.kt
+++ src/com/tdd_by_example/money/Dollar.kt
@@ -2,8 +2,6 @@ package com.tdd_by_example.money
 
 class Dollar(var amount: Int) {
 
-    fun times(multiplier: Int) {
-        amount *= multiplier
-    }
+    fun times(multiplier: Int) = Dollar(amount * multiplier)
 
 }
