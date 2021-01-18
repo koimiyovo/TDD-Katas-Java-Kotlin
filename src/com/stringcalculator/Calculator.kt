762b35a4ac5fcf8ef677f413c364428312a92922
Koimi YOVO <yovoedem33@gmail.com>
2021-01-18 23:42:17 +0100

    string calculator: Work on responsabilities in Input class

--- src/com/stringcalculator/Calculator.kt
+++ src/com/stringcalculator/Calculator.kt
@@ -1,6 +1,7 @@
 package com.stringcalculator
 
-object Calculator {
+object Calculator
+{
     fun add(input: String) =
         Input(input)
             .extractNumbers()
@@ -12,56 +13,69 @@ object Calculator {
 
     private fun List<Int>.throwExceptionOnNegativeNumbers(): List<Int> = apply {
         val negatives = filter { it < 0 }
-        if (negatives.isNotEmpty()) {
+        if (negatives.isNotEmpty())
+        {
             throw RuntimeException("Negatives not allowed: ${negatives.joinToString(",")}")
         }
     }
 }
 
-class Input(private var input: String) {
-    private val delimiters = mutableSetOf(",", "\n")
+class Input(private var value: String)
+{
+    private var separators = mutableSetOf(",", "\n")
 
-    internal fun extractNumbers(): List<Int> {
+    fun extractNumbers(): List<Int>
+    {
         this.convertEmptyInput()
-        this.extractDelimiters()
-        return input.split(delimiters).toIntList()
+        this.extractSeparators()
+        return value.split(separators).toIntList()
     }
 
-    private fun convertEmptyInput() = apply { if (input.isEmpty()) input = "0" }
+    private fun convertEmptyInput() = apply { if (value.isEmpty()) value = "0" }
 
-    private fun extractDelimiters() {
-        if (this.usesCustomDelimiterSyntax()) {
+    private fun extractSeparators()
+    {
+        if (this.usesCustomDelimiterSyntax())
+        {
             val delimiterPart = getDelimiterPart()
-            if (delimiterPart.isOfOneLength()) {
-                delimiters.add(delimiterPart)
-            } else {
-                delimiterPart.extractCustomDelimiters()
+            if (delimiterPart.isOfOneLength())
+            {
+                separators.add(delimiterPart.value)
             }
-            input = this.secondLine()
+            else
+            {
+                separators = delimiterPart.getSeparators()
+            }
+            value = this.secondLine()
         }
     }
 
-    private fun usesCustomDelimiterSyntax() = input.startsWith("//")
+    private fun usesCustomDelimiterSyntax() = value.startsWith("//")
 
-    private fun getDelimiterPart() = input.substring(input.indexOf("//") + 2, input.indexOf("\n"))
+    private fun getDelimiterPart() = Input(value.substring(value.indexOf("//") + 2, value.indexOf("\n")))
 
-    private fun String.isOfOneLength() = length == 1
+    private fun isOfOneLength() = value.length == 1
 
-    private fun String.extractCustomDelimiters() {
-        var delimiter = this
-        while (delimiter.containsDelimiter()) {
-            val oneDelimiter =
-                delimiter.substring(delimiter.indexOf("[") + 1, delimiter.indexOf("]"))
-            delimiters.add(oneDelimiter)
-            delimiter = delimiter.lookForNextDelimiter()
+    private fun getSeparators(): MutableSet<String>
+    {
+        val separators = mutableSetOf<String>()
+        while (this.containsSeparator())
+        {
+            val separator = value.substring(value.indexOf("[") + 1, value.indexOf("]"))
+            separators.add(separator)
+            lookForTheNextSeparator()
         }
+        return separators
     }
 
-    private fun String.containsDelimiter() = contains("[")
+    private fun containsSeparator() = value.contains("[")
 
-    private fun String.lookForNextDelimiter() = this.substring(this.indexOf("]") + 1)
+    private fun lookForTheNextSeparator()
+    {
+        value = value.substring(value.indexOf("]") + 1)
+    }
 
-    private fun secondLine() = input.substring(input.indexOf("\n") + 1)
+    private fun secondLine() = value.substring(value.indexOf("\n") + 1)
 
     private fun String.split(delimiters: MutableSet<String>): List<String> = split(*delimiters.toTypedArray())
 
