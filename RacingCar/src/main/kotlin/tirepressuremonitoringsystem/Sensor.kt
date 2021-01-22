603a80d5cd936613590204352007dc82c67f1556
Koimi YOVO <yovoedem33@gmail.com>
2021-01-22 21:42:22 +0100

    racing car/tire pressure: First unit test

--- RacingCar/src/main/kotlin/tirepressuremonitoringsystem/Sensor.kt
+++ RacingCar/src/main/kotlin/tirepressuremonitoringsystem/Sensor.kt
@@ -5,19 +5,22 @@ import java.util.*
 // The reading of the pressure value from the sensor is simulated in this implementation.
 // Because the focus of the exercise is on the other class.
 
-class Sensor {
-
-    fun popNextPressurePsiValue(): Double {
+open class Sensor
+{
+    open fun popNextPressurePsiValue(): Double
+    {
         val pressureTelemetryValue: Double
         pressureTelemetryValue = samplePressure()
 
         return OFFSET + pressureTelemetryValue
     }
 
-    companion object {
+    companion object
+    {
         val OFFSET = 16.0
 
-        private fun samplePressure(): Double {
+        private fun samplePressure(): Double
+        {
             // placeholder implementation that simulate a real sensor in a real tire
             val basicRandomNumbersGenerator = Random()
             return 6.0 * basicRandomNumbersGenerator.nextDouble() * basicRandomNumbersGenerator.nextDouble()
--- RacingCar/src/test/kotlin/tirepressuremonitoringsystem/AlarmTest.kt
+++ RacingCar/src/test/kotlin/tirepressuremonitoringsystem/AlarmTest.kt
@@ -2,13 +2,21 @@ package tirepressuremonitoringsystem
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 internal class AlarmTest
 {
     @Test
-    fun foo()
+    fun `when pressure is 15 then set alarm`()
     {
         val alarm = Alarm()
-        assertEquals(false, alarm.isAlarmOn)
+        val sensorMock = mock(Sensor::class.java)
+        alarm.sensor = sensorMock
+
+        `when`(alarm.sensor.popNextPressurePsiValue()).thenReturn(15.0)
+        alarm.check()
+
+        assertEquals(true, alarm.isAlarmOn)
     }
 }
